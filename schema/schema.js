c9ca325e11e9d34bfdd4788a2f782d5c8d99107e
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2023-02-14 11:56:42 -0700

    finally works

--- schema/schema.js
+++ schema/schema.js
@@ -1,53 +1,52 @@
 const graphql = require("graphql");
-const Chess = require("../models/chess");
+const mongoose = require("mongoose");
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLID,
-  GraphQLInt,
   GraphQLSchema,
   GraphQLList,
-  GraphQLNonNull,
 } = graphql;
 
-//Schema defines data on the Graph like object types(book type), relation between
-//these object types and describes how it can reach into the graph to interact with
-//the data to retrieve or mutate the data
+const ChessBoardSchema = new mongoose.Schema({
+  board: [
+    {
+      location: String,
+      piece: String,
+    },
+  ],
+});
 
-const ChessType = new GraphQLObjectType({
-  name: "chessType",
-  //We are wrapping fields in the function as we dont want to execute this ultil
-  //everything is initalized. For example below code will throw error chessType not
-  //found if not wrapped in a function
+const ChessBoardModel = mongoose.model("ChessBoards", ChessBoardSchema);
+
+const PieceType = new GraphQLObjectType({
+  name: "Piece",
   fields: () => ({
-    test: { type: GraphQLString },
-    // board: { type: new GraphQLList(GraphQLString) },
+    location: { type: GraphQLString },
+    piece: { type: GraphQLString },
   }),
 });
 
-//RootQuery describe how users can use the graph and grab data.
+const ChessBoardType = new GraphQLObjectType({
+  name: "ChessBoard",
+  fields: () => ({
+    board: { type: new GraphQLList(PieceType) },
+  }),
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    chess: {
-      type: ChessType,
-      //argument passed by the user while making the query
+    chessBoard: {
+      type: ChessBoardType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        //Here we define how to get data from database source
-
-        //this will return the chess board with id passed in argument
-        //by the user
-        return Chess.findById(args.id).then((chess) => {
-          return { test: chess.test };
-        });
+        return ChessBoardModel.findById(args.id);
       },
     },
   },
 });
 
-//Creating a new GraphQL Schema, with options query which defines query
-//we will allow users to use when they are making request.
 module.exports = new GraphQLSchema({
   query: RootQuery,
 });
